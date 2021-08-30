713a2cdd51c9c1e25a86e21bbd938ff9078cb952
Sergio Aragonés <sergioat7@gmail.com>
2021-08-31 00:01:55 +0200

    Implemented API methods

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -2,28 +2,28 @@ package com.example.android.politicalpreparedness.network
 
 import okhttp3.OkHttpClient
 
-class CivicsHttpClient: OkHttpClient() {
+class CivicsHttpClient : OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        private const val API_KEY = "AIzaSyB3l6uqYzJ27M2z2bzRGO1ayX8jC4bDgu0"
 
         fun getClient(): OkHttpClient {
             return Builder()
-                    .addInterceptor { chain ->
-                        val original = chain.request()
-                        val url = original
-                                .url()
-                                .newBuilder()
-                                .addQueryParameter("key", API_KEY)
-                                .build()
-                        val request = original
-                                .newBuilder()
-                                .url(url)
-                                .build()
-                        chain.proceed(request)
-                    }
-                    .build()
+                .addInterceptor { chain ->
+                    val original = chain.request()
+                    val url = original
+                        .url()
+                        .newBuilder()
+                        .addQueryParameter("key", API_KEY)
+                        .build()
+                    val request = original
+                        .newBuilder()
+                        .url(url)
+                        .build()
+                    chain.proceed(request)
+                }
+                .build()
         }
 
     }
