861cdb1d21fea07372996552d48f19cb96a6f4fe
Sergio Aragonés <sergioat7@gmail.com>
2021-09-04 00:04:54 +0200

    Created VoterInfoViewModel

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -1,21 +1,64 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
+class VoterInfoViewModel(
+    private val electionId: Int,
+    private val address: String,
+    private val dataSource: ElectionDao
+) : ViewModel() {
 
-    //TODO: Add live data to hold voter info
+    private val _voterInfo = MutableLiveData<VoterInfoResponse>()
+    val voterInfo: LiveData<VoterInfoResponse> = _voterInfo
 
-    //TODO: Add var and methods to populate voter info
+    private val _isElectionFollowed = MutableLiveData<Boolean>()
+    val isElectionFollowed: LiveData<Boolean> = _isElectionFollowed
 
-    //TODO: Add var and methods to support loading URLs
+    private val _url = MutableLiveData<String?>()
+    val url: LiveData<String?> = _url
 
-    //TODO: Add var and methods to save and remove elections to local database
-    //TODO: cont'd -- Populate initial state of save button to reflect proper action based on election saved status
+    private val _error = MutableLiveData<Int?>(null)
+    val error: LiveData<Int?> = _error
 
-    /**
-     * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
-     */
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            _isElectionFollowed.postValue(dataSource.getSingleElection(electionId) != null)
+            try {
+                val voterInfo = CivicsApi.retrofitService.getVoterInfo(address, electionId)
+                _voterInfo.postValue(voterInfo)
+            } catch (e: Exception) {
+                _error.postValue(R.string.generic_error)
+            }
+        }
+    }
 
+    fun setUrl(value: String?) {
+        _url.value = value
+    }
+
+    fun followUnfollowElection() {
+        val election = voterInfo.value?.election ?: return
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                if (_isElectionFollowed.value == true) {
+                    dataSource.deleteElection(voterInfo.value?.election)
+                    _isElectionFollowed.postValue(false)
+                } else {
+                    dataSource.insertElection(election)
+                    _isElectionFollowed.postValue(true)
+                }
+            } catch (e: Exception) {
+                _error.postValue(R.string.generic_error)
+            }
+        }
+    }
 }
--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModelFactory.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.example.android.politicalpreparedness.election
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.politicalpreparedness.database.ElectionDao
-import java.lang.IllegalArgumentException
 
-//TODO: Create Factory to generate VoterInfoViewModel with provided election datasource
-class VoterInfoViewModelFactory(private val dataSource: ElectionDao): ViewModelProvider.Factory {
+class VoterInfoViewModelFactory(
+    private val electionId: Int,
+    private val address: String,
+    private val dataSource: ElectionDao
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(VoterInfoViewModel::class.java)) {
-            return VoterInfoViewModel(dataSource) as T
+            return VoterInfoViewModel(electionId, address, dataSource) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
