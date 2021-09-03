a512b919392b212fb5df0a6ee410d297d67efa79
Sergio Aragonés <sergioat7@gmail.com>
2021-09-04 00:09:06 +0200

    Finished voter info screen

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -1,13 +1,20 @@
 package com.example.android.politicalpreparedness.election
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+import com.google.android.material.snackbar.Snackbar
 
 class VoterInfoFragment : Fragment() {
+
+    private lateinit var viewModel: VoterInfoViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,24 +26,34 @@ class VoterInfoFragment : Fragment() {
         val electionId = VoterInfoFragmentArgs.fromBundle(requireArguments()).argElectionId
         val division = VoterInfoFragmentArgs.fromBundle(requireArguments()).argDivision
 
-        //TODO: Add ViewModel values and create ViewModel
+        val address = "${division.country} ${division.state}"
+        val electionDao = ElectionDatabase.getInstance(requireActivity().application).electionDao
+        viewModel = ViewModelProvider(
+            this,
+            VoterInfoViewModelFactory(electionId, address, electionDao)
+        ).get(VoterInfoViewModel::class.java)
 
-        binding.fragment = this
+        binding.also {
+            it.viewModel = viewModel
+            it.lifecycleOwner = this
+        }
 
-        //TODO: Populate voter info -- hide views without provided data.
-        /**
-        Hint: You will need to ensure proper data is provided from previous fragment.
-         */
+        viewModel.url.observe(viewLifecycleOwner, {
+            openUrl(it)
+        })
 
-
-        //TODO: Handle loading of URLs
-
-        //TODO: Handle save button UI state
-        //TODO: cont'd Handle save button clicks
+        viewModel.error.observe(viewLifecycleOwner, {
+            it?.let {
+                Snackbar.make(requireView(), it, Snackbar.LENGTH_LONG).show()
+            }
+        })
 
         return binding.root
     }
 
-    //TODO: Create method to load URL intents
+    private fun openUrl(url: String?) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
 
 }
