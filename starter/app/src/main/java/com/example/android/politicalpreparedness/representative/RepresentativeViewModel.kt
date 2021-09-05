b121c5460c11bb3ca800e218ac070f8713dfd8fb
Sergio Aragonés <sergioat7@gmail.com>
2021-09-05 22:11:31 +0200

    Created RepresentativeViewModel

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,26 +1,47 @@
 package com.example.android.politicalpreparedness.representative
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.models.Address
+import com.example.android.politicalpreparedness.representative.model.Representative
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class RepresentativeViewModel: ViewModel() {
+class RepresentativeViewModel : ViewModel() {
 
-    //TODO: Establish live data for representatives and address
+    private val _address = MutableLiveData<Address?>(null)
+    val address: LiveData<Address?> = _address
 
-    //TODO: Create function to fetch representatives from API from a provided address
+    private val _representatives = MutableLiveData<List<Representative>>()
+    val representatives: LiveData<List<Representative>> = _representatives
 
-    /**
-     *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
+    fun getRepresentatives() {
 
-    val (offices, officials) = getRepresentativesDeferred.await()
-    _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
-
-    Note: getRepresentatives in the above code represents the method used to fetch data from the API
-    Note: _representatives in the above code represents the established mutable live data housing representatives
-
-     */
+        val formattedAddress = address.value?.toFormattedString() ?: return
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val representativeResponse =
+                    CivicsApi.retrofitService.getRepresentatives(formattedAddress)
+                val officials = representativeResponse.officials
+                _representatives.postValue(representativeResponse.offices.flatMap {
+                    it.getRepresentatives(
+                        officials
+                    )
+                })
+            } catch (e: Exception) {
+                //TODO:
+            }
+        }
+    }
 
     //TODO: Create function get address from geo location
 
-    //TODO: Create function to get address from individual fields
-
+    fun setAddress(line1: String, line2: String?, city: String, state: String, zip: String) {
+        _address.value = null
+        if (line1.isEmpty() && city.isEmpty() && state.isEmpty() && zip.isEmpty()) return
+        _address.value = Address(line1, line2, city, state, zip)
+    }
 }
